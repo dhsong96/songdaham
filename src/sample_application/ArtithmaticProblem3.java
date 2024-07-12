74608a5b52f71ea489bc87761b5169b4e4f4bb36
Song Da Ham <thdekgka96@naver.com>
2024-07-12 13:33:59 +0900

    주석 추가

--- src/sample_application/ArtithmaticProblem3.java
+++ src/sample_application/ArtithmaticProblem3.java
@@ -5,8 +5,7 @@ public class ArtithmaticProblem3 {
 		int celsius = 30;	// 섭씨
 		int fahrenheit = (celcius * 9/5) + 32;	// 화씨
 		
-	
-		
+		//
 		System.out.println("섭씨" + celsius + "도는 화씨" + farenheit + "도 입니다")
 		//System.out.println("섭씨를 화시로 바꾼 결과입니다. " + fahrenheit);	
 		
